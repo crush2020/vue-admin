0745ae3a840c7f6e2ccdf05f810c6b33d58ba03d
薛鉴 <1937561927@qq.com>
2020-09-04 15:06:46 +0800

    打包路径问题解决

--- build/utils.js
+++ build/utils.js
@@ -37,7 +37,12 @@ exports.cssLoaders = function(options) {
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      loaders.push(MiniCssExtractPlugin.loader)
+      loaders.push({
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          publicPath: '../../'     //此处就是要添加的内容
+        }
+      })
     } else {
       loaders.push('vue-style-loader')
     }
